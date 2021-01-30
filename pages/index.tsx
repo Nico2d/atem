38d36c46ef78024bbdf149aec8e30fdcd885212e
Nico <nico30d@gmail.com>
2021-01-30 13:12:57 +0100

    feat: add typescript

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Atem</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>
+          Welcome to <a href="https://nextjs.org">Atem</a>
+        </h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
