c53189109aaac949903d6b3b9351a05808ad3a2e
Nico <nico30d@gmail.com>
2021-02-07 23:19:23 +0100

    feat: settings-page mobile view

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -6,30 +6,70 @@ import { ThemeToggler } from "../components/molecules/themeToggler";
 import { TemplateWithSidebar } from "../components/templates/TemplateWithSidebar";
 import { useDarkMode } from "../Hooks/useDarkMode";
 import { useFontSizeSelector } from "../Hooks/useFontSizeSelector";
+import { useMediaQuery } from "../Hooks/useMediaQuery";
+import { IconKeyboardArrowRight } from "../public/icons/iconKeyboardArrowRight";
 import { device } from "../Styles/breakpoints";
 
 const Settings = () => {
   const [theme, themeToggler] = useDarkMode();
   const [fontSize, fontController] = useFontSizeSelector();
+  const isDesktop = useMediaQuery(device.tablet);
+
+  const onClickHandler = () => {
+    console.log("click");
+  };
 
   return (
     <TemplateWithSidebar>
       <PageHeading title="Ustawienia" />
       <CardContainer height="300px" width="932px">
-        Konto
-      </CardContainer>
+        <AccoutSection>
+          <div>
+            <CardHeading>Konto</CardHeading>
+            <CardDesc>Personalne informacje</CardDesc>
 
+            <CardField onClick={onClickHandler}>
+              Zmiana konta użytkownika <IconKeyboardArrowRight />
+            </CardField>
+            <CardField onClick={onClickHandler}>
+              Zmiana hasła <IconKeyboardArrowRight />
+            </CardField>
+            <CardField onClick={onClickHandler}>
+              Domyślne ustawienia <IconKeyboardArrowRight />
+            </CardField>
+            <DeleteAccount onClick={onClickHandler}>Usuń konto</DeleteAccount>
+          </div>
+
+          {isDesktop && <div>Druga sekcja</div>}
+        </AccoutSection>
+      </CardContainer>
       <CardWrapper>
         <CardContainer height="300px" width="450px">
-          Powiadomienia
+          <CardHeading>Powiadomienia </CardHeading>
+          <CardDesc>
+            Wybierz rodzaj powiadomień, które chcesz otrzymywać
+          </CardDesc>
+
+          <CardField>sprzedanych pliki</CardField>
+          <CardField>otrzymane opinie</CardField>
         </CardContainer>
+
         <CardContainer height="300px" width="450px">
-          Personalizacja
-          <ThemeToggler theme={theme} toggleTheme={themeToggler} />
-          <FontSizeSelector
-            fontSize={fontSize}
-            fontController={fontController}
-          />
+          <CardHeading>Personalizacja </CardHeading>
+          <CardDesc>Dostosuj treść i interfejs pod siebie</CardDesc>
+
+          <CardField>
+            Ciemny motyw
+            <ThemeToggler theme={theme} toggleTheme={themeToggler} />
+          </CardField>
+          <CardField>
+            Wielkość liter
+            <FontSizeSelector
+              fontSize={fontSize}
+              fontController={fontController}
+            />
+          </CardField>
+          <CardField>Język</CardField>
         </CardContainer>
       </CardWrapper>
     </TemplateWithSidebar>
@@ -38,12 +78,63 @@ const Settings = () => {
 
 export default Settings;
 
+const CardHeading = styled.h3`
+  font-size: ${({ theme }) => theme.fonts.fontSize * 1.2}rem;
+  font-weight: 400;
+`;
+
+const CardDesc = styled.p`
+  font-size: ${({ theme }) => theme.fonts.fontSize * 0.8}rem;
+  margin: 0 0 2rem;
+`;
+
+const CardField = styled.p`
+  font-size: ${({ theme }) => theme.fonts.fontSize * 0.9}rem;
+  margin-top: 2rem;
+  display: flex;
+  justify-content: space-between;
+
+  > svg {
+    fill: ${({ theme }) => theme.colors.white};
+    float: right;
+  }
+`;
+
+const DeleteAccount = styled(CardField)`
+  color: ${({ theme }) => theme.colors.red};
+  font-weight: 400;
+  position: absolute;
+  bottom: 0;
+`;
+
 const CardWrapper = styled.div`
   display: flex;
   flex-flow: column;
-  /* justify-content: center; */
 
   @media ${device.laptop} {
     flex-flow: row;
+  }
+`;
+
+const AccoutSection = styled.div`
+  display: flex;
+  flex-flow: row;
+  justify-content: space-between;
+  position: relative;
+  height: 100%;
+  flex-flow: column;
+
+  @media ${device.tablet} {
+    flex-flow: row;
+
+    ::after {
+      content: "";
+      position: absolute;
+      width: 1px;
+      height: 90%;
+      top: 5%;
+      left: 50%;
+      background: ${({ theme }) => theme.colors.linesColor};
+    }
   }
 `;
