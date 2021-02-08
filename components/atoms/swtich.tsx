27cfc0e89d6d4c4144e9feb037cf26bba69ac1b1
Nico <nico30d@gmail.com>
2021-02-08 01:01:46 +0100

    feat: add swtich button

---
+++ components/atoms/swtich.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+type SwitchProps = {
+  value?: boolean;
+  method: () => void;
+};
+
+export const Switch: React.FC<SwitchProps> = ({ value = false, method }) => {
+  return (
+    <SwitchWrapper isActive={value}>
+      <StyledInput type="checkbox" onClick={method} />
+      <Ball></Ball>
+    </SwitchWrapper>
+  );
+};
+const Ball = styled.span`
+  width: 19px;
+  height: 19px;
+  background-color: ${({ theme }) => theme.colors.cardColor};
+  border-radius: 50%;
+  display: inline-block;
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  transition: all 0.3s ease-in-out;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+const SwitchWrapper = styled.div<{ isActive: boolean }>`
+  width: 40px;
+  height: 20px;
+  border-radius: 50px;
+  display: inline-block;
+  position: relative;
+  background: ${({ theme, isActive }) =>
+    isActive ? theme.colors.primary : theme.colors.linesColor};
+  cursor: pointer;
+  transition: background-color 0.4s ease-in-out;
+  cursor: pointer;
+
+  ${Ball} {
+    left: ${({ isActive }) => (isActive ? "20px" : "1px")};
+  }
+`;
+
+const StyledInput = styled.input`
+  position: absolute;
+  left: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  z-index: 9;
+  cursor: pointer;
+`;
--- components/molecules/themeToggler.tsx
+++ components/molecules/themeToggler.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import { Button } from "../atoms/button";
 import { ThemeMode } from "../../utils/enums";
+import { Switch } from "../atoms/swtich";
 
 export const ThemeToggler = ({ theme, toggleTheme }) => {
-  return (
-    <Button
-      styleType="primary"
-      text={theme === ThemeMode.light ? "Light mode" : "Dark mode"}
-      onClick={toggleTheme}
-    />
-  );
+  return <Switch value={theme === ThemeMode.dark} method={toggleTheme} />;
 };
