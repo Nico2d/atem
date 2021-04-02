e5d88fc0893190161b30770c5ee03796eb214105
Nico <nico30d@gmail.com>
2021-04-02 12:22:39 +0200

    refactor: reduce bunch of ifs in button

--- components/atoms/button.tsx
+++ components/atoms/button.tsx
@@ -3,7 +3,6 @@ import styled from "styled-components";
 type Props = {
   text: string;
   styleType: string;
-  [x: string]: any;
   clicked?: () => void;
   type?: string;
 };
@@ -12,30 +11,34 @@ export const Button: React.FC<Props> = ({
   text,
   type,
   clicked,
-  ...props
+  styleType = "primary",
 }: Props) => {
   return (
-    <StyledButton onClick={clicked} {...props}>
+    <StyledButton
+      as={styleType === "primary" ? ButtonExtendSolid : ButtonExtendBlank}
+      onClick={clicked}
+    >
       {text}
     </StyledButton>
   );
 };
 
-const StyledButton = styled.button<Props>`
-  color: ${({ theme }) => theme.colors.fontColor};
+const StyledButton = styled.button`
   outline: none;
-  background: ${({ theme, styleType }) =>
-    styleType === "primary"
-      ? theme.colors.gradient
-      : styleType === "secondary"
-      ? "transparent"
-      : theme.colors.gradient};
+  border: none;
   padding: 1rem 4rem;
-  border: ${({ theme, styleType }) =>
-    styleType === "secondary"
-      ? `1px solid ${theme.colors.linesColor}`
-      : "none"};
   border-radius: 2rem;
   cursor: pointer;
   filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+`;
+
+export const ButtonExtendBlank = styled(StyledButton)`
+  color: ${({ theme }) => theme.colors.fontColor};
+  background: transparent;
+  border: 1px solid ${({ theme }) => theme.colors.linesColor};
+`;
+
+export const ButtonExtendSolid = styled(StyledButton)`
+  color: ${({ theme }) => theme.colors.white};
+  background: ${({ theme }) => theme.colors.gradient};
 `;
